b10d88607782d48c7738b209dfb5b0c1b975c7fc
Janek Winkler <janekx21@gmail.com>
2020-11-15 22:29:40 +0100

    get model loading working

--- resources/shader/simple_pass.vert
+++ resources/shader/simple_pass.vert
@@ -1,12 +1,11 @@
 #version 450
 
 layout(location = 0) in vec3 in_position;
-layout(location = 1) in vec3 in_color;
 
 layout(location = 0) out vec3 out_color;
 
 void main()
 {
     gl_Position = vec4(in_position, 1);
-    out_color = in_color;
+    out_color = vec3(1, 1, 1);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 #include <iostream>
+#include <engine/utils/meshloader.hpp>
 #include "engine/graphics/core/device.hpp"
 #include "engine/graphics/core/shader.hpp"
 #include "engine/graphics/core/geometrybuffer.hpp"
@@ -13,9 +14,8 @@ int main(int argc, char *argv[]) {
 
   graphics::VertexAttribute attributes[] = {
           {graphics::PrimitiveFormat::FLOAT, 3, false, false},
-          {graphics::PrimitiveFormat::FLOAT, 3, false, false}
-          };
-  auto geo = graphics::GeometryBuffer(graphics::GLPrimitiveType::TRIANGLES, attributes, 2, 0, 4 * 3 * 2 * 3);
+          // {graphics::PrimitiveFormat::FLOAT, 3, false, false}
+  };
   auto vertexShader = graphics::Shader::load("../resources/shader/simple_pass.vert", graphics::ShaderType::VERTEX,
                                              nullptr);
   auto fragmentShader = graphics::Shader::load("../resources/shader/simple_pass.frag", graphics::ShaderType::FRAGMENT,
@@ -26,11 +26,29 @@ int main(int argc, char *argv[]) {
   program.link();
   program.use();
 
-  float data[] = {-1, -1, 0, 1, 0, 0,
-                  1, -1, 0, 0, 1, 0,
-                  0, 1, 0, 0, 0, 1};
-  geo.bind();
-  geo.setData(data, 4 * 3 * 2 * 3);
+  auto geo = graphics::GeometryBuffer(graphics::GLPrimitiveType::TRIANGLES, attributes, 1, 1);
+
+  auto meshData = utils::MeshLoader::get("models/crate.obj");
+  std::vector<float> positionData(meshData->positions.size() * 3);
+  int j = 0;
+  for (auto &position : meshData->positions) {
+    positionData[j] = position.x;
+    positionData[j + 1] = position.y;
+    positionData[j + 2] = position.z;
+    j += 3;
+  }
+
+  geo.setData(positionData.data(), positionData.size() * sizeof(float));
+
+  std::vector<int> indexData(meshData->faces.size() * 3);
+  int i = 0;
+  for (auto &face : meshData->faces) {
+    indexData[i] = face.indices[0].positionIdx;
+    indexData[i + 1] = face.indices[1].positionIdx;
+    indexData[i + 2] = face.indices[2].positionIdx;
+    i += 3;
+  }
+  geo.setIndexData(indexData.data(), sizeof(int) * indexData.size());
   glClearColor(.25, .2, .2, 1);
 
   while (!glfwWindowShouldClose(window)) {
