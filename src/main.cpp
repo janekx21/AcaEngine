0464b21ffd5ba82fd54d4719f2cc83f98a0e958f
Janek Winkler <janekx21@gmail.com>
2020-11-19 18:32:41 +0100

    rotated move vector

--- src/main.cpp
+++ src/main.cpp
@@ -8,34 +8,32 @@
 #include "engine/graphics/core/shader.hpp"
 #include "engine/graphics/camera.hpp"
 #include <glm/gtx/transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <engine/input/inputmanager.hpp>
 #include <engine/math/directions.h>
 #include <map>
+#include <iostream>
+#include <thread>
+
+using namespace std::chrono_literals;
 
 class Flyer {
 public:
   void update(float dt) {
-    auto move = math::zero;
-    for(auto& [key, direction] : keyToDirection) {
-      if (input::InputManager::isKeyPressed(key)) {
-        move += direction;
-      }
-    }
-    if (glm::length(move) > 1) {
-      move = glm::normalize(move);
-    }
-    if (glm::length(velocity) > 0) {
-      velocity += -glm::normalize(velocity) * glm::length(velocity) * friction * dt;
-    }
-
-    velocity += move * acceleration * dt;
-    position += velocity * dt;
+    applyRotation();
+    applyFriction(dt);
+    auto move = getMove();
+    move = glm::inverse(rotation) * move;
+    applyAcceleration(move, dt);
   }
 
   glm::vec3 getPosition() { return position; }
+  glm::quat getRotation() { return rotation; }
 
 private:
   glm::vec3 position = math::zero;
+  glm::quat rotation = glm::identity<glm::quat>();
   glm::vec3 velocity = math::zero;
   const float friction = 10.f;
   const float acceleration = 100.f;
@@ -45,6 +43,36 @@ private:
           {input::Key::A, math::left},
           {input::Key::D, math::right},
   };
+
+  glm::vec3 getMove() {
+    auto move = math::zero;
+    for (auto&[key, direction] : keyToDirection) {
+      if (input::InputManager::isKeyPressed(key)) {
+        move += direction;
+      }
+    }
+    if (glm::length(move) > 1) {
+      move = glm::normalize(move);
+    }
+    return move;
+  }
+
+  void applyFriction(float dt) {
+    if (glm::length(velocity) > 0) {
+      velocity += -glm::normalize(velocity) * glm::length(velocity) * friction * dt;
+    }
+  }
+
+  void applyAcceleration(glm::vec3 move, float dt) {
+    velocity += move * acceleration * dt;
+    position += velocity * dt;
+  }
+
+  void applyRotation() {
+    auto deltaMouse = input::InputManager::getDeltaCursorPos();
+    auto deltaRot = glm::angleAxis(deltaMouse.x * .01f, math::up);
+    rotation *= deltaRot;
+  }
 };
 
 class TimeManager {
@@ -60,7 +88,7 @@ public:
     return elapsed.count();
   }
 
-  float getDeltaTime() {return deltaTime;}
+  float getDeltaTime() { return deltaTime; }
 
   void resetDeltaTime() {
     auto now = clock::now();
@@ -95,13 +123,13 @@ int main(int argc, char *argv[]) {
   auto texture = graphics::Texture2D::load("../resources/textures/planet1.png", sampler, false);
   auto mesh = graphics::Mesh("models/sphere.obj");
 
-  auto camera = graphics::Camera(90, .1, 100);
+  auto camera = graphics::Camera(90, .1f, 100);
 
   auto flyer = Flyer();
   auto time = TimeManager();
 
   // permanent settings
-  glClearColor(.25, .2, .2, 1);
+  glClearColor(.25f, .2f, .2f, 1);
   texture->bind(0);
   program.use();
   auto mvpLocation = program.getUniformLoc("c_modelViewProjection");
@@ -114,7 +142,7 @@ int main(int argc, char *argv[]) {
 
     flyer.update(dt);
 
-    camera.setView(glm::translate(-flyer.getPosition()));
+    camera.setView(glm::mat4(flyer.getRotation()) * glm::translate(-flyer.getPosition()));
 
     auto modelMatrix = glm::translate(glm::vec3(0, 0, -3.0))
                        * glm::rotate(glm::pi<float>() * time.getTime() * .3f, glm::vec3(0, 1, 0));
@@ -127,6 +155,8 @@ int main(int argc, char *argv[]) {
 
     glfwSwapBuffers(window);
     time.resetDeltaTime();
+    input::InputManager::update();
+    std::this_thread::sleep_for(10ms);
   }
 
   graphics::Device::close();
